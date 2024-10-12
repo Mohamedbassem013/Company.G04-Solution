cafd94049ae5140354e0a81a753a9603d5c4b44a
Muhammed Bassem <Muhammed Bassem@DESKTOP-DA9SS6B>
2024-10-13 00:07:59 +0300

    Add project files.

---
+++ Company.G04.PL/Helper/EmailSettings.cs
@@ -0,0 +1,23 @@
+﻿using Company.G04.DAL.Model;
+using System.Net;
+using System.Net.Mail;
+
+namespace Company.G04.PL.Helper
+{
+    public static class EmailSettings
+    {
+        public static void SendEmail (Email email) 
+        {
+            //Mail Server : gmail.com 
+            //Smtp
+
+            var client = new SmtpClient ("smtp.gmail.com",587);
+
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential("mohammedatta095@gmail.com", "ecnqvfkiqraxoorz");
+
+            client.Send("mohammedatta095@gmail.com", email.To, email.Subjets, email.Body);
+        }
+    }
+}
